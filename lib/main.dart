5bc51bdcda95985792e38e1c3b3952bee8807557
brockeh8 <bfreiberger1@student.gsu.edu>
2025-11-05 23:49:59 -0500

    fire store service

--- lib/main.dart
+++ lib/main.dart
@@ -43,3 +43,46 @@ class Item {
     );
   }
 }
+
+class FirestoreService {
+  FirestoreService._();
+  static final FirestoreService instance = FirestoreService._();
+
+  final _col = FirebaseFirestore.instance.collection('items');
+
+  Future<void> addItem(Item item) async => _col.add(item.toMap());
+
+  Stream<List<Item>> getItemsStream() => _col
+      .orderBy('createdAt', descending: true)
+      .snapshots()
+      .map((s) => s.docs.map((d) => Item.fromMap(d.id, d.data())).toList());
+
+  Future<void> updateItem(Item item) async {
+    if (item.id == null) return;
+    await _col.doc(item.id!).update(item.toMap());
+  }
+
+  Future<void> deleteItem(String id) async => _col.doc(id).delete();
+}
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  runApp(const InventoryApp());
+}
+
+class InventoryApp extends StatelessWidget {
+  const InventoryApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Inventory Management App',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      routes: {
+        '/': (_) => const InventoryHomePage(title: 'Inventory Home Page'),
+        AddEditItemScreen.routeName: (_) => const AddEditItemScreen(),
+        DashboardScreen.routeName: (_) => const DashboardScreen(),
+      },
+    );
+  }
+}
